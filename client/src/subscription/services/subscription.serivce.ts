9dc1327f47ec5e3c0ed7b8577542a85acfe2435b
Nikky K.O <nikky@192.168.1.174>
2023-10-15 23:31:40 +0300

    master: Updated code

--- client/src/subscription/services/subscription.serivce.ts
+++ client/src/subscription/services/subscription.serivce.ts
@@ -25,7 +25,6 @@ export class SubscriptionService implements OnModuleInit {
   public getUser(user: UserRequest): Observable<User> {
     return this.subscriptionService.getUser(user).pipe(
       map((res) => {
-        console.log(res);
         return res;
       }),
     );
